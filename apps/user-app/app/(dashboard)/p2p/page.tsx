a88ece820ad5280a1f0d2210fbda88cf397a0160
Rohit Tegginamath <rohittegginamath78@gmail.com>
2026-07-07 14:15:05 +0530

    added hello

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -3,5 +3,6 @@ import { SendCard } from "../../../components/SendCard";
 export default function() {
     return <div className="w-full"> 
         <SendCard />
+        <h1>hello</h1>
     </div>
 }
